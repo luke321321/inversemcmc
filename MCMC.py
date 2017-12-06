8376820dcf8b42453fa59cea1637735246d760b0
Luke Dyer <luke321321@gmail.com>
2017-12-06 16:15:32 +0000

    Slightly quicker method for M-H (sped up by x2)

--- MCMC.py
+++ MCMC.py
@@ -7,29 +7,34 @@ Created on Wed Dec  6 14:33:52 2017
 import numpy as np
 import math, random
 import matplotlib.pyplot as plt
+import time
 
 def MHRandomWalk(distribution,length,speed=0.5,x0=0,burnTime=200):
     x = np.zeros(burnTime + length)
     x[0] = x0
+    distOld = distribution(x0)
     for i in range(1,burnTime + length):
         y = x[i-1] + random.normalvariate(0,speed)
-        u = random.random()
-        if(u <= acceptance(distribution,x[i-1],y)):
+        if(distOld == 0): #Accept whatever
             x[i] = y
+            distOld = distribution(y)
         else:
-            x[i] = x[i-1]
+            u = random.random()
+            distNew = distribution(y)
+            if(u <= min(1,distNew/distOld)): #check acceptance
+                x[i] = y
+                distOld = distNew
+            else:
+                x[i] = x[i-1]
     return x
-    
-def acceptance(distribution,X,Y):
-    if(distribution(X) == 0):
-        return 1
-    else:
-        alpha = distribution(Y)/distribution(X)
-        return min(1,alpha)
 
 def normalDist(x):
     return math.exp(-x**2/2)/math.sqrt(2*math.pi)
 
-x = MHRandomWalk(normalDist, length=10000, speed=0.5)
+t0 = time.clock()
+x = MHRandomWalk(normalDist, length=100000, speed=0.5)
+t1 = time.clock()
+print('CPU time for loops in Python OLD METHOD:', t1-t0)
+
 plt.hist(x, normed=True, bins=77)
 plt.show()
