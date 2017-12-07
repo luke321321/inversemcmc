442b279f94b35fd611380c1cd1ced962c78c9186
Luke Dyer <luke321321@gmail.com>
2017-12-07 15:59:54 +0000

    Creates Gaussian  process by just using mean and runs MCMC to sample distributions  - using dummy kernels, dummy G and 1 dim parameter space

--- MCMC.py
+++ MCMC.py
@@ -8,52 +8,103 @@ Will later optimise using Numba/Cython
 """
 import numpy as np
 import math, random
-#import matplotlib.pyplot as plt
-#import time
+import matplotlib.pyplot as plt
+import time
 
-def MHRandomWalk(distribution,length,speed=0.5,x0=0,burnTime=200):
+def MHRandomWalk(density,length,speed=0.5,x0=0,burnTime=200):
     x = np.zeros(burnTime + length)
+    rvNormal = speed*np.random.randn(burnTime+length)
     x[0] = x0
-    distOld = distribution(x0)
+    densityOld = density(x0)
     for i in range(1,burnTime + length):
-        y = x[i-1] + random.normalvariate(0,speed)
-        if(distOld == 0): #Accept whatever
+        y = x[i-1] + rvNormal[i]
+        if(densityOld == 0): #Accept whatever
             x[i] = y
-            distOld = distribution(y)
+            densityOld = density(y)
         else:
             u = random.random()
-            distNew = distribution(y)
-            if(u <= min(1,distNew/distOld)): #check acceptance
+            densityNew = density(y)
+            if(u <= min(1,densityNew/densityOld)): #check acceptance
                 x[i] = y
-                distOld = distNew
+                densityOld = densityNew
             else:
                 x[i] = x[i-1]
-    return x
+    return x[200:]
+
 
 root2Pi = math.sqrt(2*math.pi)
-normalDist = lambda x: math.exp(-x**2/2)/root2Pi
+normalDensity = lambda x: math.exp(-x**2/2)/root2Pi
 
-#Create a dummy kernal, just l^2 norm
-kernal = lambda x,y: np.linalg.norm(x-y)
-
-def createGaussianEmulator(phi,N,kernal,designPoints):
+def createGaussianEmulator(phi,kernal,designPoints):
     #Kernal has to be able to give out same shape as it gets in
     phiStar = phi(designPoints)
     #Create K_*
-    kernalStar = kernal(np.meshgrid(phiStar,phiStar)) #NxN matrix
+    kernalStar = kernal(np.meshgrid(designPoints,designPoints)[0],
+                        np.meshgrid(designPoints,designPoints)[1]) #NxN matrix
     kernalStarInverse = np.linalg.inv(kernalStar)
-    k = lambda u: kernal(u,phiStar) #N vector
-    mean = k @ kernalStarInverse @ phiStar
+#    k = lambda u: kernal(u,phiStar) #N vector
+    kernalStarInverseDotPhiStar = kernalStarInverse @ phiStar
+    #might need to have this vectorised for more dimensions
+    mean = lambda u: kernal(u,designPoints) @ kernalStarInverseDotPhiStar
     #kernalN = lambda x,y: kernal(x,y) - k(y) @ kernalStarInverse @ k(x)
     
     #At the moment just doing the simple case for \Phi_N(u) = mean(u)
     return mean     
     
 
-#t0 = time.clock()
-#x = MHRandomWalk(normalDist, length=1000000, speed=0.5)
-#t1 = time.clock()
-#print('CPU time for loops in Python:', t1-t0)
-#
-#plt.hist(x, normed=True, bins=77)
+#%%Test case with easy phi.  G = identity
+#First neeed to generate some data y
+#setup
+np.random.seed(1564)
+sigma = 1
+dimensionU = 1
+length = 10**5 #length of random walk in MCMC
+numberDesignPoints = 100
+speedRandomWalk = 0.5
+
+#Generate data
+u = np.random.randn(dimensionU)
+y = u + sigma*np.random.standard_normal(u.shape)
+
+phi = np.vectorize(lambda u: np.linalg.norm(y-u)/(2*sigma))
+#Create a dummy kernal, just l^2 norm
+kernal = np.vectorize(lambda x,y: np.linalg.norm(x-y))
+
+#N design points in [-2,2]
+designPoints = np.linspace(-2,2,numberDesignPoints)
+GP = createGaussianEmulator(phi,kernal,designPoints)
+
+#Plotting phi and GP of phi:
+#t = np.arange(-2,2,0.1)
+#plt.plot(t,GP(t))
+#plt.plot(t,phi(t), color='green')
+#plt.plot(designPoints,phi(designPoints), 'ro')
 #plt.show()
+
+#%% Calculations
+densityPrior = lambda u: normalDensity(u)*np.exp(-phi(u))
+densityPost = lambda u: normalDensity(u)*np.exp(-GP(u))
+
+print('Running MCMC with length:', length)
+t0 = time.clock()
+distPrior = MHRandomWalk(densityPrior, length, speed=speedRandomWalk)
+t1 = time.clock()
+print('CPU time calculating distPrior:', t1-t0)
+
+t0 = time.clock()
+distPost = MHRandomWalk(densityPost, length, speed=speedRandomWalk)
+t1 = time.clock()
+print('CPU time calculating distPost:', t1-t0)
+
+#Timing if want it:
+#t0 = time.clock()
+#x = MHRandom1Walk(normalDensity, length=1000000, speed=0.5)
+#t1 = time.clock()
+#print('CPU time for loops in Python: non-vectorised', t1-t0)
+
+
+#%% Plotting distributions of Prior and Post
+plt.hist(distPrior, bins=77, alpha=0.5, density=True, label='Prior')
+plt.hist(distPost, bins=77, alpha=0.5, density=True, label='Post')
+plt.legend(loc='upper right')
+plt.show()
