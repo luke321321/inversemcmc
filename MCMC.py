3b48bd413bc14bb48cb6ac6f08e9f8bd0a2e9400
Luke Dyer <luke321321@gmail.com>
2017-12-11 10:27:18 +0000

    Now working in n-dim and plotting working

--- MCMC.py
+++ MCMC.py
@@ -12,11 +12,15 @@ import matplotlib.pyplot as plt
 import time
 
 #We pass dimension via x0
-def MHRandomWalk(density,length,speed=0.5,x0=0,burnTime=200):
-    x = np.zeros((burnTime + length, x0.size))
+def MHRandomWalk(density,length,speed=0.5,x0=np.array([0]),burnTime=200):
+    if isinstance(x0, np.ndarray):
+        dim = x0.size
+    else:
+        dim = 1
+        
+    x = np.zeros((burnTime + length, dim))
     #Generates normal rv in R^n of length=speed or 0.
-    rvNormal = speed*normalizeVector(np.random.randn(burnTime + length, x0.size))
-    #TODO update so that it's multi-dimensional
+    rvNormal = speed*normalizeVector(np.random.randn(burnTime + length, dim))
 
     x[0] = x0
     densityOld = density(x0)
@@ -67,30 +71,19 @@ def createGaussianEmulator(phi,kernal,designPoints):
     return mean     
 
 def normalizeVector(a):
-    """Normalises the vector a but keeps 0 vectors as 0 vectors"""
+    """Normalises the vector 'a' but keeps 0 vectors as 0 vectors"""
     norm = np.sum(a*a, 1)
     for x in np.nditer(norm,op_flags=['readwrite']):
         if(x == 0):
             x[...] = 1
     return a/(np.sqrt(norm)[:,None])
 
-def create_ranges_nd(start, stop, N, endpoint=True):
-    """Creates n-dim range of numbers using broadcasting - like n-dim linspace
-    based off https://stackoverflow.com/questions/46694167/vectorized-numpy-linspace-across-multi-dimensional-arrays"""
-    if endpoint==1:
-        divisor = N-1
-    else:
-        divisor = N
-    steps = (1.0/divisor) * (stop - start)
-    return start[...,None] + steps[...,None]*np.arange(N)
-
 #%%Test case with easy phi.  G = identity
 #First neeed to generate some data y
 #setup
-np.random.seed(1564)
 sigma = 1
-dimU = 1
-length = 10**4 #length of random walk in MCMC
+dimU = 2
+length = 10**5 #length of random walk in MCMC
 numberDesignPoints = 5 #in each dimension
 speedRandomWalk = 0.5
 #End points of n-dim lattice for the design points
@@ -101,7 +94,7 @@ maxRange = 2
 u = np.random.randn(dimU)
 y = u + sigma*np.random.standard_normal(u.shape)
 
-phi = np.linalg.norm(y-u)/(2*sigma)
+phi = lambda u : np.linalg.norm(y-u)/(2*sigma)
 v1phi = np.vectorize(lambda u: np.linalg.norm(y-u)/(2*sigma))
 v2phi = np.vectorize(lambda u: np.linalg.norm(y-u)/(2*sigma), signature='(i)->()')
 #Create a dummy kernal, just l^2 norm.  Vectorised with the correct signature
@@ -118,43 +111,64 @@ else:
     designPoints = np.linspace(minRange,maxRange,numberDesignPoints)
     GP = createGaussianEmulator(v1phi,kernal,designPoints)
 
-vGP = np.vectorize(lambda u: GP(u))
+
 #Plotting phi and GP of phi:
-t = np.arange(-2,2,0.1)
-plt.plot(t,vGP(t))
-plt.plot(t,v1phi(t), color='green')
-plt.plot(designPoints,phi(designPoints), 'ro')
-plt.show()
+plotFlag = 0
+if plotFlag == 1:      
+    #Vectorise GP for plotting
+    vGP = np.vectorize(lambda u: GP(u), signature='(i)->()')
+    if dimU == 1:
+        #This is just 1d plot
+        t = np.linspace(-2,2,10)
+        plt.plot(t,vGP(t))
+        #plt.plot(t,v2phi(t), color='green')
+        plt.plot(designPoints,v1phi(designPoints), 'ro')
+        plt.show()
+    elif dimU == 2:
+        X = np.linspace(minRange, maxRange, 10)
+        Y = np.linspace(minRange, maxRange, 10)
+        Z = np.hstack(np.meshgrid(X, Y)).swapaxes(0,1).reshape(2,-1).T
+        X = Z[:,0]
+        Y = Z[:,1]
+        
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        #Plot the surface
+        ax.plot_trisurf(X, Y, vGP(Z), antialiased=True)
+        #Plot the design points
+        ax.scatter(designPoints[:,0], designPoints[:,1], v2phi(designPoints), color='green')
+        plt.show()
+
 
 #%% Calculations
 densityPrior = lambda u: normalDensity(u)*np.exp(-phi(u))
 densityPost = lambda u: normalDensity(u)*np.exp(-GP(u))
 
+x0 = np.zeros(dimU)
 print('Running MCMC with length:', length)
 t0 = time.clock()
-distPrior = MHRandomWalk(densityPrior, length, speed=speedRandomWalk)
+distPrior = MHRandomWalk(densityPrior, length, x0=x0, speed=speedRandomWalk)
 t1 = time.clock()
 print('CPU time calculating distPrior:', t1-t0)
 
 t0 = time.clock()
-distPost = MHRandomWalk(densityPost, length, speed=speedRandomWalk)
+distPost = MHRandomWalk(densityPost, length, x0=x0, speed=speedRandomWalk)
 t1 = time.clock()
 print('CPU time calculating distPost:', t1-t0)
 
 
 
-
 #%% Plotting distributions of Prior and Post
-#plt.hist(distPrior, bins=77, alpha=0.5, density=True, label='Prior')
-#plt.hist(distPost, bins=77, alpha=0.5, density=True, label='Post')
-#plt.legend(loc='upper right')
-#plt.show()
-
-
-
-
-
-
+if dimU == 1:
+    plt.hist(distPrior, bins=77, alpha=0.5, density=True, label='Prior')
+    plt.hist(distPost, bins=77, alpha=0.5, density=True, label='Post')
+    plt.legend(loc='upper right')
+    plt.show()
+elif dimU == 2:
+    plt.hist(distPrior[:,0], bins=77, alpha=0.5, density=True, label='Prior')
+    plt.hist(distPost[:,0], bins=77, alpha=0.5, density=True, label='Post')
+    plt.legend(loc='upper right')
+    plt.show()
 
 #%% Tests
 
